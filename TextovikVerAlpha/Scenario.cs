0c049b573168e9dbda1ecacb0238acf734d62b82
artemon235 <artemon353233@gmail.com>
2020-02-10 14:47:41 +0300

    10.02.2020

---
+++ TextovikVerAlpha/Scenario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Reflection;
+using System.IO;
+
+namespace TextovikVerAlpha
+{
+    class Scenario
+    {
+        public void Save()
+        { }
+    }
+    class Scene
+    {
+
+    }
+    class ImageEditor
+    {
+        public List<Image> Images { get; set; }
+        public void AddImage(string pathToFile, string pathToSave)
+        {
+            if (pathToFile != null)
+            {
+                File.Copy(pathToFile, pathToSave);
+            }
+            else { }
+            
+        }
+        public void AddImage(string[] pathToFiles, string pathToSave)
+        {
+            if (pathToFiles != null)
+            {
+                foreach (string path in pathToFiles)
+                {
+                    File.Copy(path, pathToSave);
+                }
+            }
+        }
+        public void DeleteImage(Image img)
+        {
+            Images.Remove(img);
+        }
+
+    }
+    class AudioEditor
+    {
+
+    }
+    class TextEditor
+    {
+
+    }
+}
